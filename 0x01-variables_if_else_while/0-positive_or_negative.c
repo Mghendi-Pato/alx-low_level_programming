3072518765e8a0b4a8dcab6fa5f1bc87c44bca4b
Mghendi-Pato <mwamburipatrick8@gmail.com>
2022-09-09 14:52:54 -0700

    Addedd betty changes

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-/* more headers goes there */
+/**
+ * main -entry of the program
+ *
+ * Return zero
+ */
 
-/* betty style doc for function main goes there */
 int main(void)
 {
 	int n;
