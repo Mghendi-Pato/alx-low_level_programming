efb6b8245730c2e9f748236dc8d55e215fd9da9b
Mghendi-Pato <mwamburipatrick8@gmail.com>
2022-09-15 15:39:53 -0700

    added all

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,15 +1,19 @@
+/*
+ * File: 1-alphabet.c
+ * Auth: Emmah Moraa
+ */
+
 #include "main.h"
 
 /**
  * print_alphabet - prints the alphabet in lowercase, followed by a new line.
  */
-
-void  print_alphabet(void)
+void print_alphabet(void)
 {
-	char alphabet;
-	for (alphabet = "a"; alphabet <= "z"; alphabet ++)
-	{
-		printf("%c", alphabet);
-	}
-	return (0);
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+		_putchar(letter);
+
+	_putchar('\n');
 }
