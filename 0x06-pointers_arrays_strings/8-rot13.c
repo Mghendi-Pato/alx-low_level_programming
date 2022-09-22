93d3752da7e2b524d45f9b4038ad2ebe6f6b7e8c
Mghendi Pato <mwamburipatrick8@gmail.com>
2022-09-22 21:25:26 +0300

    Added all

--- 0x06-pointers_arrays_strings/8-rot13.c
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -1,47 +1,32 @@
-#include "main.h"
+#include "holberton.h"
 
 /**
- * rot13 - Encodes a string using rot13.
- * @str: The string to be encoded.
+ *rot13 - codes a string insto rot 13
  *
- * Return: A pointer to the encoded string.
+ *@s: the pointer containing the string
+ *
+ *Return: the string already coded
  */
-char *rot13(char *str)
+char *rot13(char *s)
 {
-	int indx1 = 0, indx2;
-	char alphabet[52] = {'A', 'B', 'C', 'D', 'E', 'F',
-				'G', 'H', 'I', 'J', 'K', 'L',
-				'M', 'N', 'O', 'P', 'Q', 'R',
-				'S', 'T', 'U', 'V', 'W', 'X',
-				'Y', 'Z', 'a', 'b', 'c', 'd',
-				'e', 'f', 'g', 'h', 'i', 'j',
-				'k', 'l', 'm', 'n', 'o', 'p',
-				'q', 'r', 's', 't', 'u', 'v',
-				'w', 'x', 'y', 'z'};
-	char rot13key[52] = {'N', 'O', 'P', 'Q', 'R', 'S',
-				'T', 'U', 'V', 'W', 'X', 'Y',
-				'Z', 'A', 'B', 'C', 'D', 'E',
-				'F', 'G', 'H', 'I', 'J', 'K',
-				'L', 'M', 'n', 'o', 'p', 'q',
-				'r', 's', 't', 'u', 'v', 'w',
-				'x', 'y', 'z', 'a', 'b', 'c',
-				'd', 'e', 'f', 'g', 'h', 'i',
-				'j', 'k', 'l', 'm'};
+	char a1[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char a2[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+	int i = 0;
+	int c;
 
-	while (str[indx1])
+	while (s[i] != 0)
 	{
-		for (indx2 = 0; indx2 < 52; indx2++)
+		c = 0;
+		while (a1[c] != 0)
 		{
-			if (str[indx1] == alphabet[indx2])
+			if (s[i] == a1[c])
 			{
-				str[indx1] = rot13key[indx2];
+				s[i] = a2[c];
 				break;
 			}
+			c++;
 		}
-
-		indx1++;
+		i++;
 	}
-
-	return (str);
+	return (s);
 }
-
